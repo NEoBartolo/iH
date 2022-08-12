95872e48c0acf2c2ae86f88866ce2e52a7e61a9f
Эо Бартоло <EoBartolo@tutanota.com>
2022-08-12 23:40:06 +0300

    Add files via upload

---
+++ store.php
@@ -0,0 +1,17 @@
+<?php
+include ('buttons/drug.btn.php');
+include ('buttons/loc.btn.php');
+include ('buttons/store.php');
+include ('buttons/common.php');
+
+$mMKb = [[$CCGBtn]];
+$vitMKb = [[$SAaBtn]];
+$secMKb = [[$RBtkBtn,$RCntBtn],[$RTcrBtn,$RRwkBtn]];
+$wMKb = [[$WEnOsBtn],[$WEnLoBtn]];
+$payMKb = [[$ltcBtn,$btcBtn],[$maibBtn]];
+$payKb = [[$payBtn]];
+$okKb = [[$okBut],[$noBut]];
+$helpMKb = [[$helpBtn]];
+$MKb = [['МЕНЮ'],[$xchBtn,$rulBtn]];
+$banMKb = [['❌ БАН (60 сек.) ❌'.PHP_EOL.'нажми чтобы обновить']];
+?>
