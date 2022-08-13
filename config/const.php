139f97e161ef6cc43c3115a625da270b6af48656
Эо Бартоло <EoBartolo@tutanota.com>
2022-08-13 11:31:43 +0300

    Add files via upload

---
+++ config/const.php
@@ -0,0 +1,4 @@
+<?php
+$api = $apiL.'bot'.$token;
+include('server/engine.comp.php');
+?>
---
+++ config/init.php
@@ -0,0 +1,7 @@
+<?php
+include ('payment/litecoin.php');
+include ('payment/bitcoin.php');
+include ('payment/maib.php');
+
+include ('server/connect.php');
+?>
