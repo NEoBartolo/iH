3c2041fe560a1e87c91ca996bf5c4c501b29269b
Эо Бартоло <EoBartolo@tutanota.com>
2022-08-13 11:36:01 +0300

    Add files via upload

---
+++ library/store/drug.store.php
@@ -0,0 +1,26 @@
+<?php
+$APVPS = 'Alpha-PVP';
+$EnzoS = 'ENZO Gold';
+$MDMAS = 'MDMA в кр.';
+$MtdnS = 'Metadon кр.';
+$MephS = 'Mephedrone';
+$AmphS = 'Amphetamine';
+$CocaS = 'Cocaine 98%';
+$MrjnS = 'Marijuana 40/60';
+$ExtcS = 'XTC "Tesla"';
+$LSDS = 'марки LSD';
+$NBOMS = 'марки NBOMe';
+$RegaS = 'Реагент JE32';
+$SyrcS = 'Сырец (героин)';
+$PsylS = 'Грибы (псилоцибиновые)';
+$TramS = 'Трамадол';
+$ChimS = 'Кур.микс (хим)';
+$SibrS = 'Сибирь';
+$SbtxS = 'Субутекс';
+$GernS = 'Героин';
+$AlprS = 'Алпразолам';
+$TarnS = 'ТАРЕН';
+$GashS = 'Гашиш';
+$TvrdS = 'ТВ (синт.гаш)';
+$AbstS = 'Абсент';
+?>
