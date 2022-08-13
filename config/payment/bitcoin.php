1b8e7ea0ea1bf93d86aba0bacf37f1a9d1f14548
Эо Бартоло <EoBartolo@tutanota.com>
2022-08-13 11:30:25 +0300

    Add files via upload

---
+++ config/payment/bitcoin.php
@@ -0,0 +1,5 @@
+<?php
+$btc = 'bc1qktl4p3j9cudfcp77s6c9mwpduxk7804vcclhkt';
+$osBtc = '0.00064573';
+$loBtc = '0.00111928';
+?>
---
+++ config/payment/litecoin.php
@@ -0,0 +1,5 @@
+<?php
+$ltc ='ltc1qmzhfx0gd9my963wtceqe2x9tj03n7tx8f4wja3';
+$osPrc = '0.25476';
+$loPrc = '0.43888';
+?>
